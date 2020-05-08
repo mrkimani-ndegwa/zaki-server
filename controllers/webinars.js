3f683ada2542da8b3a48ca3a8958388dec507611
Kimani Ndegwa <kimani@pave.solutions>
2020-05-08 17:59:20 +0300

    Pass data through req.body as it should be.

--- controllers/webinars.js
+++ controllers/webinars.js
@@ -7,20 +7,6 @@ const token = generateSignedToken();
 const create_webinar = async (req, res) => {
     // TODO: Create Logic
     try {
-
-    // TODO: Use AJV for validation here. This could get messy.
-    // Test data for now but should be retrieved from the req.body
-    // This currently works lol!
-    const payLoad = {
-        "topic": "ZAKI Test Webinar 2",
-        "type": ZOOM_WEBINAR_TYPE,
-        "start_time": "2020-09-20T06:59:00Z",
-        "duration": "60",
-        "timezone": "America/Los_Angeles",
-        "password": "pass123",
-        "agenda": "Zaki Testing API Format 2"
-    };
-    console.log(req.body, "here")
     //Store the options for Zoom API which will be used to make an API call later.
     const options = {
         //You can use a different uri if you're making an API call to a different Zoom endpoint.
@@ -33,14 +19,12 @@ const create_webinar = async (req, res) => {
             'User-Agent': 'Zoom-api-Jwt-Request',
             'content-type': 'application/json'
         },
-        body: payLoad,
+        body: JSON.stringify(req.body),
         json: true //Parse the JSON string in the response
     };
 
     const response = await rp(options);
     res.send({msg: "Success", data: response})
-
-    // throw new Error("Response not ok.")
     } catch(e){
         res.send ({ msg: `${e.message}` })
     }
