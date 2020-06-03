0064b1ceb597da46b4fd2d82761534788a65e2d2
Kimani Ndegwa <kimani@pave.solutions>
2020-06-03 20:51:22 +0300

    update endpoints list

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,15 @@ const Router = require('express').Router();
 const { 
     create_webinar,
     list_webinars,
-    list_webinar_campaigns_on_actionkit
+    get_webinar,
+    list_webinar_campaigns_on_actionkit,
+    update_webinar_details
 } = require("../controllers/webinars");
+
+const {
+    addWebinarRegistrants,
+    listWebinarRegistrants
+} = require("../controllers/registrants")
 
 const {
     getAccessToken
@@ -29,6 +36,12 @@ Router.get("/timezones", getTimeZonesList);
 // Webinar Routes
 Router.post('/webinars', authenticateJWT, create_webinar);
 Router.get('/webinars', authenticateJWT, list_webinars);
+Router.patch('/webinars/:webinarId', authenticateJWT, update_webinar_details);
+Router.get('/webinars/:webinarId', authenticateJWT, get_webinar);
+
+Router.post('/webinars/:webinarId/registrants', authenticateJWT, addWebinarRegistrants);
+Router.get('/webinars/:webinarId/registrants', authenticateJWT, listWebinarRegistrants);
+
 Router.get("/ak-webinar-campaigns", authenticateJWT, list_webinar_campaigns_on_actionkit);
 
 module.exports = Router;
