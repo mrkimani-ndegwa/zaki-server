d0d71bbf9a6f6ff1210efa4505e3b536a09e249c
Kimani Ndegwa <kimani@pave.solutions>
2020-06-17 13:46:26 +0300

    Cleanup

--- controllers/webinar-webhooks.js
+++ controllers/webinar-webhooks.js
@@ -20,27 +20,12 @@ const webhookListeners = (req, res) => {
         default:
             return false
     }
-    
-    /** 
-     * SAMPLE RESPONSE FROM THIS DUDE
-    {
-         "event": "webinar.updated",
-         "payload": {
-         "account_id": "R15o7-M4RO6l3yNf2Jp0rw",
-         "operator": "webinars@350.org",
-         "operator_id": "6HNRlExjQcisy1iZVwl8EA",
-         "object": { "id": 93280627326, "topic": "More testing nope yup" },
-         "old_object": { "id": 93280627326, "topic": "More testing nope" },
-         "time_stamp": 1591692737657
-         }
-    }
-    */
+
 
 
 };
 
 
-//  { "event": "webinar.updated", "payload": { "account_id": "R15o7-M4RO6l3yNf2Jp0rw", "operator": "webinars@350.org", "operator_id": "6HNRlExjQcisy1iZVwl8EA", "object": { "id": 95133448898, "topic": "Testing Webhooks Faling to you honesty" }, "old_object": { "id": 95133448898, "topic": "Testing Webhooks" }, "time_stamp": 1592388379733 } }
 
 
 
